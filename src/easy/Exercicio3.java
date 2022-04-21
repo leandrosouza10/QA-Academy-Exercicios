68a333690c100c7e0739a0428ba5e1cdd457587c
Leandro <souzaleandro79@gmail.com>
2022-04-21 19:11:09 -0300

    Exercicio3 Easy - Refatoração

--- src/easy/Exercicio3.java
+++ src/easy/Exercicio3.java
@@ -1,25 +1,16 @@
 package easy;
 
-import javax.swing.JOptionPane;
-
 /*
     Faça um algoritmo para ler dois valores,
     armazenar em variáveis, e exibir os valores das variáveis, trocados.
 */
 public class Exercicio3 {
 
-    public static void main(String[] args) {
-
-        String primeiroValor, segundoValor;
-
-        primeiroValor = JOptionPane.showInputDialog("Digite o primeiro valor: ");
-        segundoValor = JOptionPane.showInputDialog("Digite o  segundo valor: ");
-
-        System.out.println("======== Valores trocados ===========");
-
-        // valores trocados
-        System.out.println("Primeiro valor digitado: " + segundoValor);
-        System.out.println("Segundo valor digitado: " + primeiroValor);
+    public String valoresTrocados(String primeiroValor, String segundoValor) {
+      
+        return "Primeiro valor digitado: " + segundoValor + "\n"
+        + "Segundo valor digitado: " +  primeiroValor;
+      
 
     }
 
---
+++ src/execucaoRetornoExerciciosEasy/Execucao3.java
@@ -0,0 +1,22 @@
+package execucaoRetornoExerciciosEasy;
+
+import javax.swing.JOptionPane;
+
+
+import easy. Exercicio3;
+
+public class Execucao3 {
+    public static void main(String[] args) {
+
+        Exercicio3 exercicio3 = new Exercicio3(); // instanciando a classe Exercicio 3 easy
+
+       String primeiroValor = JOptionPane.showInputDialog("Digite o primeiro valor: ");
+       String segundoValor = JOptionPane.showInputDialog("Digite o segundo valor: ");
+       
+       System.out.println(exercicio3.valoresTrocados(primeiroValor, segundoValor));//faço a chamada e o valor
+        
+     
+       
+    }
+    
+}
