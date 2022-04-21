cad5334568acc58146a2fded025f1bba14603320
Leandro <souzaleandro79@gmail.com>
2022-04-21 18:16:07 -0300

    Transformação Exercicio2 Easy - metodo com retorno e com  passagem de parametros

--- src/easy/Exercicio2.java
+++ src/easy/Exercicio2.java
@@ -1,14 +1,13 @@
+
 package easy;
 
 //Faça um algoritmo para ler e exibir uma palavra.
 
-import javax.swing.JOptionPane;
 
 public class Exercicio2 {
-    public static void main(String[] args) {
 
-        String palavraDigitada;
-        palavraDigitada = JOptionPane.showInputDialog("Digite uma palavra! ");
-        System.out.println("A palavra digitada foi: " + palavraDigitada);
+    public String palavraDigitada (String palavra) {      
+        
+        return "A palavra digitada foi: " + palavra;
     }
 }
---
+++ src/execucaoRetornoExerciciosEasy/Execucao2.java
@@ -0,0 +1,16 @@
+package execucaoRetornoExerciciosEasy;
+
+import easy. Exercicio2 ;
+
+public class Execucao2 {  
+        public static void main(String[] args) {
+            Exercicio2 exercicio2 = new Exercicio2();
+            String palavra = exercicio2.palavraDigitada("Humanos");
+            System.out.println(palavra);
+            
+        }   
+        
+    }
+    
+    
+
