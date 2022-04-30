384f766879e27447d3a58edfe7f5b8dadd630bb6
Leandro <souzaleandro79@gmail.com>
2022-04-30 19:22:31 -0300

    Exercicio6 Medium - Refatoração

--- src/execucaoExerciciosMedium/Execucao6.java
+++ src/execucaoExerciciosMedium/Execucao6.java
@@ -1,5 +1,14 @@
 package execucaoExerciciosMedium;
+import javax.swing.JOptionPane;
+
+import medium.Exercicio6;
 
 public class Execucao6 {
-    
+    public static void main(String[] args) {
+        String fatorialDigitadoStg = JOptionPane.showInputDialog("Digite um número para calcular o fatorial: ");
+        int fatorial = Integer.parseInt(fatorialDigitadoStg);
+        Exercicio6 exercicio6 = new Exercicio6();
+        fatorial = exercicio6.calculaFatorial(fatorial);
+        System.out.println("O valor do fatorial é: " + fatorial );   
+    }        
 }
--- src/medium/Exercicio6.java
+++ src/medium/Exercicio6.java
@@ -1,17 +1,16 @@
 package medium;
 
-import javax.swing.JOptionPane;
+
 
 public class Exercicio6 {
-    public static void main(String[] args) {
-        int fatorial, i;
-        fatorial = Integer.parseInt(JOptionPane.showInputDialog("Digite um número para calcular o fatorial: "));
+    public int calculaFatorial(int fatorial) {
+        int i;      
         i = fatorial;
         while (i > 1) {
             fatorial = fatorial * (i - 1);
             i--;
-        }
-        System.out.println("O valor do fatorial é: " + fatorial);
+        }       
+        return fatorial;
     }
 
 }
