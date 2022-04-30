1318895889764d59dd8623567f2803ec902ebbbc
Leandro <souzaleandro79@gmail.com>
2022-04-30 10:53:30 -0300

    Exercicio8 Easy - Refatoração

--- src/easy/Exercicio8.java
+++ src/easy/Exercicio8.java
@@ -1,29 +1,31 @@
 package easy;
 
-import javax.swing.JOptionPane;
-
 public class Exercicio8 {
-    public static void main(String[] args) {
 
-        double salario, ir;
+    public double calculaImpostoDeRenda (double salario) {
+         double ir = 0;   
 
-        salario = Double.parseDouble(JOptionPane.showInputDialog("Digite o valor do seu salário: R$ "));
-        if (salario >= 0 && salario <= 1903.98) {
-            System.out.println("Isento de imposto renda:");
+        if (salario <= 1903.98) {
+            ir = 0;            
+            
         } else if (salario >= 1903.99 && salario <= 2826.65) {
-            ir = (salario * 0.075) - 142.80;
-            System.out.println("O valor do imposto de renda será: R$ " + ir);
+            ir = (salario * 0.075) - 142.80;           
+         
         } else if (salario >= 2826.66 && salario <= 3751.05) {
             ir = (salario * 0.15) - 354.80;
-            System.out.println("O valor do imposto de renda será: R$ " + ir);
+         
         } else if (salario >= 3751.06 && salario <= 4664.68) {
-            ir = (salario * 0.225) - 636.13;
-            System.out.println("O valor do imposto de renda será: R$ " + ir);
+            ir = (salario * 0.225) - 636.13;        
+           
         } else if (salario > 4664.68) {
-            ir = (salario * 0.275) - 869.36;
-            System.out.println("O valor do imposto de renda será: R$ " + ir);
+            ir = (salario * 0.275) - 869.36;                  
 
         }
+        return ir;
+    }
+    
+    public double calculaSalarioLiquido(double salario, double ir) {
+        return salario - ir;
 
     }
 
---
+++ src/execucaoRetornoExerciciosEasy/Execucao8.java
@@ -0,0 +1,16 @@
+package execucaoRetornoExerciciosEasy;
+import javax.swing.JOptionPane;
+import easy.Exercicio8;
+
+public class Execucao8 {
+    public static void main(String[] args) {
+       String baseSalario = JOptionPane.showInputDialog("Informe o valor do seu salário: R$ ");
+       double salario = Double.parseDouble(baseSalario);
+       Exercicio8 exercicio8 = new Exercicio8();
+       System.out.println("O imposto de renda será de R$ " + exercicio8.calculaImpostoDeRenda(salario)) ;
+        double ir = exercicio8.calculaImpostoDeRenda(salario);
+       System.out.println("O salário liquido será de R$ " + exercicio8.calculaSalarioLiquido(salario,ir));
+       
+    }
+    
+}
