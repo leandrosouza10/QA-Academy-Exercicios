dcdf35dc3ad2576f226e2d6ba702a8db826a891c
Leandro <souzaleandro79@gmail.com>
2022-04-30 19:52:30 -0300

    Exercicio7_8 Medium - Refatoração

---
+++ src/execucaoExerciciosMedium/Execucao7_8.java
@@ -0,0 +1,11 @@
+package execucaoExerciciosMedium;
+import medium.Exercicio7_8;
+
+public class Execucao7_8 {
+    public static void main(String[] args) {
+        
+        Exercicio7_8 exercicio7_8 = new Exercicio7_8();
+        String[] vetMeses = exercicio7_8.criaVetorMeses();
+        exercicio7_8.imprimeValorVetor(vetMeses);
+    }    
+}
