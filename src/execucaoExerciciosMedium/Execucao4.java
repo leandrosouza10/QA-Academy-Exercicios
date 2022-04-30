bcedf9788a69aa4aaad7ffc569605f314a4d2f05
Leandro <souzaleandro79@gmail.com>
2022-04-30 16:49:25 -0300

    Exercicio4 Medium - Refatoração

--- src/execucaoExerciciosMedium/Execucao4.java
+++ src/execucaoExerciciosMedium/Execucao4.java
@@ -1,5 +1,12 @@
 package execucaoExerciciosMedium;
 
+import medium.Exercicio4;
+
 public class Execucao4 {
+    public static void main(String[] args) {
+        Exercicio4 exercicio4 = new Exercicio4();
+        exercicio4.mulitiplicacaoDeNumeros();
+
+    }
     
 }
