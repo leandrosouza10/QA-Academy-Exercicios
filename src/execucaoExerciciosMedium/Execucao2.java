8cddb5511a66366ce096fa74b69e95039fbec2b9
Leandro <souzaleandro79@gmail.com>
2022-04-30 16:25:04 -0300

    Exercicio2 Medium - Refatoração

--- src/execucaoExerciciosMedium/Execucao2.java
+++ src/execucaoExerciciosMedium/Execucao2.java
@@ -1,5 +1,21 @@
 package execucaoExerciciosMedium;
+import javax.swing.JOptionPane;
+
+import medium.Exercicio2;
 
 public class Execucao2 {
+    public static void main(String[] args) {
+        String valorInvestido = JOptionPane.showInputDialog("Informe o valor do investimento? R$: ");
+        double valorInvestimento = Double.parseDouble(valorInvestido);
+        Exercicio2 exercicio2 = new Exercicio2();
+        exercicio2.calculaInvestimentoJurosCompostos(valorInvestimento);
+
+        double valorJuros = exercicio2.calculaInvestimentoJurosCompostos(valorInvestimento);
+        double valorTotal = exercicio2.calculaValorTotalDoInvestimento(valorInvestimento, valorJuros);
+        System.out.println("Valor do investimento: R$ " + valorInvestimento );  
+        System.out.println("Valor do juros é: R$ " + valorJuros);
+        System.out.println("Valor total do investimento: R$ " + valorTotal);
+       
+    }
     
 }
--- src/medium/Exercicio2.java
+++ src/medium/Exercicio2.java
@@ -1,26 +1,20 @@
 package medium;
 
-import javax.swing.JOptionPane;
-
 public class Exercicio2 {
-    public static void main(String[] args) {
+    public double calculaInvestimentoJurosCompostos( double valorInvestimento) {
 
-        double valorInvestido, txJuros = 0.12, total, valorJuros;
-        int i = 1;
-
-        valorInvestido = Double.parseDouble(JOptionPane.showInputDialog("Qual o valor do investimento? R$: "));
-        total = valorInvestido;
-
-        while (i <= 10) {
-            total = (total * txJuros) + total;
-            i++;
+        double txJuros = 0.05, totalAtualizadoComJuros;
+        int ano = 1, tempoInvestido = 10;
+        totalAtualizadoComJuros = valorInvestimento;
+        while (ano <= tempoInvestido) {
+            totalAtualizadoComJuros = (totalAtualizadoComJuros * txJuros) + totalAtualizadoComJuros;
+            ano++;
         }
-        valorJuros = total - valorInvestido;
+        return totalAtualizadoComJuros - valorInvestimento;
+    }// fim
 
-        System.out.println("Valor Investido: R$ " + valorInvestido);
-        System.out.println("Valor dos juros: R$ " + valorJuros);
-        System.out.println("Valor total: R$ " + total);
-
+    public double calculaValorTotalDoInvestimento(double valorInvestimento, double valorJuros) {
+        return  valorInvestimento + valorJuros;
     }
 
 }
