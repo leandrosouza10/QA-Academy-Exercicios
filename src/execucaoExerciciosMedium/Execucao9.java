908852df4b67edcada0820f6181d096ffaed1bcd
Leandro <souzaleandro79@gmail.com>
2022-04-30 21:00:37 -0300

    Exercicio9 Medium - Refatoração

--- src/execucaoExerciciosMedium/Execucao9.java
+++ src/execucaoExerciciosMedium/Execucao9.java
@@ -1,5 +1,9 @@
 package execucaoExerciciosMedium;
+import medium.Exercicio9;
 
 public class Execucao9 {
-    
+    public static void main(String[] args) {
+        Exercicio9 exercicio9 =  new Exercicio9();
+        exercicio9.nomeNumeroAlunos();        
+    }    
 }
--- src/medium/Exercicio9.java
+++ src/medium/Exercicio9.java
@@ -3,22 +3,32 @@ package medium;
 import javax.swing.JOptionPane;
 
 public class Exercicio9 {
-    public static void main(String[] args) {
-        String[] nomeAlunos = new String[7];
-        int[] numeracaoAlunos = new int[7];
-        int i = 0;
+        public void nomeNumeroAlunos() {
+            String[] nomeAlunos = new String[7];
+            int[] numeracaoAlunos = new int[7];
+            int i = 0;
 
-        while (i < 7) {
-            nomeAlunos[i] = JOptionPane.showInputDialog("Digite o nome do aluno: ");
-            numeracaoAlunos[i] = Integer.parseInt(JOptionPane.showInputDialog("Digite o número do aluno: "));
-            i++;
+            while(i < 7){
+                nomeAlunos [i] = recebeAluno();
+                numeracaoAlunos [i] = recebeNumeroAluno();
+                i++; 
+            }
+            imprimeNomeNumero(nomeAlunos,numeracaoAlunos);
+        }        
+
+        private void imprimeNomeNumero(String [] vetorNomes, int [] vetorAlunos) {
+            int i = 0;
+            while (i < 7) {
+                System.out.println("Nome do aluno: " + vetorNomes[i] + " - " + " número do aluno: " + vetorAlunos[i]);
+                i++;
+            }
         }
-        i = 0;
-        while (i < 7) {
-            System.out.println("Nome do aluno: " + nomeAlunos[i] + " - " + " número do aluno: " + numeracaoAlunos[i]);
-            i++;
 
+        private String recebeAluno() {
+            return JOptionPane.showInputDialog("Digite o nome do aluno: ");
         }
-    }
 
-}
+        private int recebeNumeroAluno() {
+            return  Integer.parseInt(JOptionPane.showInputDialog("Digite o número do Aluno:"));
+        }
+    }    
